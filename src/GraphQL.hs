0d73d7382ddb08d3e19a3e9e03f6a651168787e0
Kirill Chayka <kirillfx@gmail.com>
2022-04-22 19:48:59 +0300

    feat: simplified structure

--- src/GraphQL.hs
+++ src/GraphQL.hs
@@ -50,22 +50,40 @@ newtype TextId = TextId Text
 
 makeRelativeToProject "schema/schema.gql" >>= importGQLDocument
 
+mkDivBlock :: Monad m => DivId -> [Block m] -> Block m
+mkDivBlock i xs = 
+  BlockDivBlock $ DivBlock (pure i) (pure xs)
+
+mkTextBlock :: Monad m => TextId -> Text -> Block m
+mkTextBlock i content = 
+  BlockTextBlock $ TextBlock (pure i) (pure content)
+
+
+-- data Context = Context {
+--   blocks :: Map TextId TextBlock m
+-- }
+
+
 -- samplePage :: GetPageArgs -> IO (Maybe (Page IO))
-samplePage :: MonadIO m => Arg "id" (Maybe PageId) -> m (Maybe (Page m))
+samplePage :: forall m. Monad m => MonadIO m => Arg "id" (Maybe PageId) -> m (Maybe (Page m))
 -- samplePage = error "Not implemented"
 samplePage _  = pure . Just $ Page
   (pure (PageId "page0"))
   (pure "Page 0")
   -- (error "Not implemented")
-  (pure
-    [ BlockTextBlock $ TextBlock (pure $ TextId "text0") (pure "Content of block 0")
-    , BlockDivBlock $ DivBlock (pure $ DivId "div0") (pure
-      [ BlockTextBlock $ TextBlock (pure $ TextId "text2") (pure "Content of block 1")
-      , BlockTextBlock $ TextBlock (pure $ TextId "text2") (pure "Content of block 2")
-      ])
-    ]
-  )
-
+  (pure $ mkDivBlock (DivId "0") subBlocks)
+  where
+    subBlocks :: [Block m]
+    subBlocks = 
+      [ BlockTextBlock $ TextBlock (pure $ TextId "text0") (pure "Content of block 0")
+      , BlockDivBlock $ DivBlock (pure $ DivId "div0") (pure
+        [ mkTextBlock (TextId "text1") "TextBlock1"
+        , mkTextBlock (TextId "text2") "TextBlock2"
+        , mkDivBlock (DivId "div2") 
+          [ mkTextBlock (TextId "text3") "TextBlock3" 
+          ]
+        ])
+      ]
 
 rootResolver :: RootResolver IO () Query Undefined Undefined
 rootResolver =
